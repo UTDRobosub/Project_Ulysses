c0fecfbcd2549f5cdb3a5c51ee7d3d443204d75b
iburami <cu.iburami@gmail.com>
2017-03-21 22:06:39 -0500

    added a class

---
+++ Code/Libraries/Controller.cpp
@@ -0,0 +1,37 @@
+#include "Controller.h"
+
+Controller::Controller(){
+    //ctor
+}
+
+Controller::~Controller(){
+    //dtor
+}
+
+void Controller::moveForward(int in,int motor){
+
+}
+
+void Controller::moveBack(int in, int motor){
+
+}
+
+void Controller::turnLeft(int in, int motor){
+
+}
+
+void Controller::turnRight(int in, int motor){
+
+}
+
+void Controller::strafeLeft(int in, int motor){
+
+}
+
+void Controller::strafeRight(int in, int motor){
+
+}
+
+void Controller::turnMotors(int in, int motor){
+
+}
---
+++ Code/Libraries/Controller.h
@@ -0,0 +1,33 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+/*3/16/2017
+The following functions need to be made: moveForward(), moveBack(), turnLeft(),
+turnRight(), strafeLeft(), strafeRight() using the turnMotors() function in the
+arduino recent test sketch. If we could make class for these functions that would be
+ideal. It would be helpful for us later, when using beaglebone. please use C/C++.
+
+We also need to make a generic PID class, I will upload an example to within the
+libraries folder. If you would like to more info an PID loops please go the Follow
+website: (http://brettbeauregard.com/blog/2012/01/arduino-pid-autotune-library/).
+If you find better resource for this, please share it with me (text it to me).
+
+Sonar senors stuff is coming soon, so please do some research into digital signal processing.*/
+
+class Controller
+{
+    public:
+        Controller();
+        virtual ~Controller();
+        void moveForward(int,int);
+        void moveBack(int,int);
+        void turnLeft(int,int);
+        void turnRight(int,int);
+        void strafeLeft(int,int);
+        void strafeRight(int,int);
+        void turnMotors(int,int);
+
+    private:
+};
+
+#endif // CONTROLLER_H
